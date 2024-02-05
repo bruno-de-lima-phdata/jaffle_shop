9c2125f99699b09db7aa40aeed2be607a91dda4d
Bruno Souza de Lima <bsouzadelima@phdata.io>
2024-02-05 16:30:17 -0300

    orders.sql: adding group by date

--- models/orders.sql
+++ models/orders.sql
@@ -1,4 +1,4 @@
-{% set payment_methods = ['credit_card', 'coupon', 'bank_transfer', 'gift_card'] %}
+{% set payment_methods = ['cash', 'credit_card', 'coupon', 'bank_transfer', 'gift_card'] %}
 
 with orders as (
 
@@ -25,7 +25,7 @@ order_payments as (
 
     from payments
 
-    group by order_id
+    group by order_id, day
 
 ),
 
