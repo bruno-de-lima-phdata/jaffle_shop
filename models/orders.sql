be6139e70aa8d7b0e782eea9da06be4e370b6384
Bruno Souza de Lima <bsouzadelima@phdata.io>
2024-02-05 16:33:08 -0300

    orders.sql: adding 'store' column

--- models/orders.sql
+++ models/orders.sql
@@ -20,7 +20,7 @@ order_payments as (
         {% for payment_method in payment_methods -%}
         sum(case when payment_method = '{{ payment_method }}' then amount else 0 end) as {{ payment_method }}_amount,
         {% endfor -%}
-
+    store a
         sum(amount) as total_amount
 
     from payments
